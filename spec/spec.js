37623cd9e2509e084a21edd018ea45ac19270bf7
J-C <jcjoneskern@gmail.com>
2017-04-18 21:47:40 -0400

    toBe != toEqual

--- spec/spec.js
+++ spec/spec.js
@@ -2,15 +2,15 @@
 
 describe("flattener", function() {
   it("removes brackets from [[1,2,3],4]", function() {
-    expect(flatten([[1,2,3],4])).toBe([1,2,3,4]);
+    expect(flatten([[1,2,3],4])).toEqual([1,2,3,4]);
   });
   it("removes brackets from [[1,2,[3]],4]", function() {
-    expect(flatten([[1,2,[3]],4])).toBe([1,2,3,4]);
+    expect(flatten([[1,2,[3]],4])).toEqual([1,2,3,4]);
   });
   it("removes brackets from [[1,[[2]],[3]],4]", function() {
-    expect(flatten([[1,[[2]],[3]],4])).toBe([1,2,3,4]);
+    expect(flatten([[1,[[2]],[3]],4])).toEqual([1,2,3,4]);
   });
   it("also works with words", function() {
-    expect(flatten(['apple', ['banana', 'pear']])).toBe(['apple', 'banana', 'pear']);
+    expect(flatten(['apple', ['banana', 'pear']])).toEqual(['apple', 'banana', 'pear']);
   });
 });
